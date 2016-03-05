fbaf2e0f2e835e8c47dcd757952b7767007854b1
Talha215 <bmw215t@aim.com>
2016-03-05 16:40:14 -0500

    commit everything

---
+++ RPSText/RPSText/RPSText.cpp
@@ -0,0 +1,14 @@
+// RPSText.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#define ROCK          1
+#define PAPER         2
+#define SCISSORS      3
+
+int main()
+{
+	
+    return 0;
+}
+
