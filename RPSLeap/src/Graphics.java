db953dc1f48f346453aae387b928719d881e465a
Nate Hilscher <thisisnotnate@gmail.com>
2016-03-05 19:37:23 -0500

    JFrame Class

---
+++ RPSLeap/src/Graphics.java
@@ -0,0 +1,26 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+ 
+public class Graphics {
+    private static void createAndShowGUI() {
+        JFrame frame = new JFrame("Rock Paper Scissors");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 
+        JLabel emptyLabel = new JLabel("");
+        emptyLabel.setPreferredSize(new Dimension(1280, 720));
+        frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+ 
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+ 
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+}
