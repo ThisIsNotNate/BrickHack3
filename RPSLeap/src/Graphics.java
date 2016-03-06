a0a70cdaf2293236fa33b01d605a7e59db480ca5
Talha215 <bmw215t@aim.com>
2016-03-05 19:55:36 -0500

    1920x1080

--- RPSLeap/src/Graphics.java
+++ RPSLeap/src/Graphics.java
@@ -8,7 +8,7 @@ public class Graphics {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
  
         JLabel emptyLabel = new JLabel("");
-        emptyLabel.setPreferredSize(new Dimension(1280, 720));
+        emptyLabel.setPreferredSize(new Dimension(1920, 1080));
         frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
  
         //Display the window.
